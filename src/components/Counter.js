cd6335ced5fc525d292358772956f55c22a5f4b9
andxbes <andxbes@gmail.com>
2025-01-12 21:22:59 +0200

    322

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,13 @@
-import { counterActions } from '../store';
+
+import { counterActions } from '../store/counter';
 import classes from './Counter.module.css';
 import { useSelector, useDispatch } from 'react-redux';
 
 const Counter = () => {
 
   const dispatch = useDispatch();
-  const counter = useSelector(state => state.counter);
-  const show = useSelector(state => state.showCounter);
+  const counter = useSelector(state => state.counter.value);
+  const show = useSelector(state => state.counter.showCounter);
 
   const incrementHandler = () => {
     dispatch(counterActions.increment());
